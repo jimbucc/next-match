461ea4013d4d847cb6eed4e29550e45fe24ee931
jimbucc <James.Bucciferro@PERKINELMER.COM>
2024-07-07 07:05:06 -0400

    End of section 2

---
+++ src/app/members/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const MembersPage = () => {
+  return (
+    <div>
+        <h3 className="text-3xl">This is the Members Page</h3>
+        <Link href='/'>Go back home</Link>
+        
+    </div>
+  )
+}
+export default MembersPage
---
+++ src/components/navbar/NavLink.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { NavbarItem } from '@nextui-org/react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+type Props = {
+  href: string
+  label: string
+}
+
+const NavLink = ({ href, label }: Props) => {
+  const pathName = usePathname()
+  return (
+    <NavbarItem isActive={pathName === href} as={Link} href={href}>
+      {label}
+    </NavbarItem>
+  )
+}
+export default NavLink
